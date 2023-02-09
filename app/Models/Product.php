03f71bf562d51176515c8bdbf41b1ee0cb3a423c
vasil14 <vasil_v@hotmail.com>
2023-02-09 15:53:37 +0100

    Config Product Model and Migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id', 'mpn', 'price', 'sale_price', 'vip_price', 'stock', 'availability', 'taglia', 'parent_id', 'title', 'description', 'link', 'image_link', 'product_type', 'eta', 'marche', 'genere', 'personaggi', 'colore'];
 }
--- database/migrations/2023_02_09_144829_create_products_table.php
+++ database/migrations/2023_02_09_144829_create_products_table.php
@@ -15,6 +15,24 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->integer('mpn');
+            $table->float('price')->nullable();
+            $table->float('sale_price')->nullable();
+            $table->float('vip_price')->nullable();
+            $table->string('stock')->nullable();
+            $table->string('availability');
+            $table->string('taglia');
+            $table->integer('parent_id')->nullable();
+            $table->string('title');
+            $table->text('description');
+            $table->string('link');
+            $table->string('image_link');
+            $table->text('product_type');
+            $table->integer('eta')->nullable();
+            $table->string('marche');
+            $table->string('genere');
+            $table->string('personaggi')->nullable();
+            $table->string('colore');
             $table->timestamps();
         });
     }
