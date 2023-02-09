81330129184606d22bee19cc25cf5f43cc141f00
vasil14 <vasil_v@hotmail.com>
2023-02-09 15:43:43 +0100

    Config User's Model and Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastName');
             $table->string('email')->unique();
+            $table->string('gender');
+            $table->date('birthday')->date_format('Y-m-d');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
