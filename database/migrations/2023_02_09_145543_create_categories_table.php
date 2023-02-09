bfd41fd33ebd432b60de6785d19afd97e603ab41
vasil14 <vasil_v@hotmail.com>
2023-02-09 15:59:11 +0100

    Config Category Model and Migration

--- database/migrations/2023_02_09_145543_create_categories_table.php
+++ database/migrations/2023_02_09_145543_create_categories_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('_id');
+            $table->string('name');
+            $table->integer('parent_id')->unsigned()->nullable();
             $table->timestamps();
         });
     }
