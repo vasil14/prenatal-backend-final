471940624370f418f2e3a675d8a01c864222fa4d
vasil14 <vasil_v@hotmail.com>
2023-02-10 10:25:21 +0100

    importing controllers in api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 // User routes
 Route::prefix('user')->group(
